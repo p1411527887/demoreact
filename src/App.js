06aa963e8c3c9a2e7f39268cef62a2597ff22142
ankitjangidx <martinxaj@gmail.com>
2023-02-11 17:20:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://media.geeksforgeeks.org/wp-content/uploads/geeksforgeeks-13.png" className="App-logo" alt="GFG-logo" />
+     
       </header>
     </div>
   );
